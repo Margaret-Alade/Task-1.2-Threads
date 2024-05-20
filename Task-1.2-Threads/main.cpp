5f1cb3bbfd5f52da42d449cc1b80e8cc00289c0f
Margaret Alade <alade.margaret@yandex.ru>
2024-05-20 22:55:10 +0300

    Initial Commit

---
+++ Task-1.2-Threads/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-1.2-Threads
+//
+//  Created by Маргарет  on 20.05.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
